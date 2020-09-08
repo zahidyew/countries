7241c0cd49ddaa46a3034cf01fe2972d070b8f76
zahidyew <zahidyew96@gmail.com>
2020-09-09 04:05:59 +0800

    Updates

---
+++ precache-manifest.ba144862c1f56c23012b112f0aba9e71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef373ebd9f3d58553ee01d9fb4e57ae1",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "92e24586eb310afbc7a8",
+    "url": "/countries/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ebf234beda1f3316d1d8",
+    "url": "/countries/static/css/main.b1e698d9.chunk.css"
+  },
+  {
+    "revision": "92e24586eb310afbc7a8",
+    "url": "/countries/static/js/2.75d9db3f.chunk.js"
+  },
+  {
+    "revision": "80fbca8adb3bddfb5d2b4da02e1c4f6e",
+    "url": "/countries/static/js/2.75d9db3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf234beda1f3316d1d8",
+    "url": "/countries/static/js/main.b429d591.chunk.js"
+  },
+  {
+    "revision": "98a2b82ebb3a74cbde09",
+    "url": "/countries/static/js/runtime-main.e148dddc.js"
+  }
+]);
